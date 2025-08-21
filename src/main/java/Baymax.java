2f1a49928ff7a517b5e3fb0a592b713b9ea107d0
Jovan Teo <jovanty61@gmail.com>
2025-08-22 02:34:37 +0800

    Level-1: Improve chatbot introduction and add echo functionality
    The program only prints a greeting message and cannot echo user input.
    
    Add echo functionality to echo user input until 'bye' is entered.

--- src/main/java/Baymax.java
+++ src/main/java/Baymax.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
 public class Baymax {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         final String HORIZONTAL = "__________________________________";
-        System.out.printf("%s\nHello! I'm Baymax\nWhat can I do for you?\n%s\nBye. Hope to see you again soon!\n"
+        System.out.printf("%s\nHello! I'm Baymax\nWhat can I do for you?\n%s\n"
                 ,HORIZONTAL, HORIZONTAL);
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.printf("%s\nBye. Hope to see you again soon!", HORIZONTAL);
+                break;
+            } else {
+                System.out.printf("%s\n%s\n%s\n", HORIZONTAL, input, HORIZONTAL);
+            }
+        }
     }
 }
