53ea80a6cb67bceeef14dc5b0139e67e96bc7c4f
Jovan Teo <jovanty61@gmail.com>
2025-08-22 02:59:27 +0800

    Level-2: Implement add and list functionality
    User input is echoed but not stored and cannot be displayed
    
    Add a String array of fixed size 100 to store user inputs.
    Implement 'list' command to display stored items.

--- src/main/java/Baymax.java
+++ src/main/java/Baymax.java
@@ -1,19 +1,34 @@
 import java.util.Scanner;
+
 public class Baymax {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        int j = 0;
+        final String[] list = new String[100];
         final String HORIZONTAL = "__________________________________";
+
         System.out.printf("%s\nHello! I'm Baymax\nWhat can I do for you?\n%s\n"
-                ,HORIZONTAL, HORIZONTAL);
+                , HORIZONTAL, HORIZONTAL);
 
         while (true) {
             String input = scanner.nextLine();
+
+            list[j] = input;
+            j++;
+
             if (input.equals("bye")) {
                 System.out.printf("%s\nBye. Hope to see you again soon!", HORIZONTAL);
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < j - 1; i++) {
+                    System.out.printf((i + 1) + ". %s\n", list[i]);
+                }
+                System.out.println(HORIZONTAL);
             } else {
-                System.out.printf("%s\n%s\n%s\n", HORIZONTAL, input, HORIZONTAL);
-            }
+
+            System.out.printf("%s\nadded: %s\n%s\n", HORIZONTAL, input, HORIZONTAL);
         }
     }
+}
 }
