7963b78cc2eb8a1cb9a7f40a5b5c59676edc18b2
Jovan Teo <jovanty61@gmail.com>
2025-08-22 04:21:17 +0800

    Level-4: Remove taskId and static id field from Task class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,14 +1,10 @@
 public class Task {
     protected String taskName;
     protected boolean isDone;
-    protected int taskId;
-    private static int id = 0;
 
     public Task(String taskName) {
         this.taskName = taskName;
         this.isDone = false;
-        id++;
-        this.taskId = id;
     }
 
     public void markAsDone() {
