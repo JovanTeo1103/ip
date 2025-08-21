9c68dcf5555fbc3ec623acb17a25977bfd77fbc6
Jovan Teo <jovanty61@gmail.com>
2025-08-22 02:13:13 +0800

    Level-0: Rename main class and add greeting
    The main class has a default name and no greeting message is displayed.
    
    Rename the main class to Baymax.
    Add a greeting message printed to console when the program starts.

---
+++ src/main/java/Baymax.java
@@ -0,0 +1,7 @@
+public class Baymax {
+    public static void main(String[] args) {
+        final String HORIZONTAL = "__________________________________";
+        System.out.printf("%s\nHello! I'm Baymax\nWhat can I do for you?\n%s\nBye. Hope to see you again soon!\n"
+                ,HORIZONTAL, HORIZONTAL);
+    }
+}
