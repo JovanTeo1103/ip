af6ec40728294125f04d358a84fb675f552693b1
Jovan Teo <jovanty61@gmail.com>
2025-09-03 16:59:44 +0800

    Level-10: Add files from javafx repo
    Baymax had no GUI functionalities
    
    Main class added is used to build the GUI interface

---
+++ src/main/java/baymax/Main.java
@@ -0,0 +1,34 @@
+package baymax;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * A GUI for Duke using FXML.
+ */
+public class Main extends Application {
+
+    private Baymax baymax = new Baymax();
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            stage.setMinHeight(220);
+            stage.setMinWidth(417);
+
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setBaymax(baymax);  // inject the Duke instance
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
